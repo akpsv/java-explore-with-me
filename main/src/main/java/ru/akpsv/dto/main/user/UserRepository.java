0170e29dcd88553da7c366561ccdcd46afe42075
Алексей Копосов <alexkopos@ya.ru>
2023-02-10 19:03:55 +0300

    feat: Реализовать api для работы с пользователями

---
+++ main/src/main/java/ru/akpsv/dto/main/user/UserRepository.java
@@ -0,0 +1,8 @@
+package ru.akpsv.dto.main.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
